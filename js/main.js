d1d6d9653258ebc46df70c1df0e440ae4791b6c7
Hyeji-nam <hyeji5245@gmail.com>
2022-12-08 18:58:19 +0900

    1차 완성

---
+++ js/main.js
@@ -0,0 +1,30 @@
+import Dropdown from "bootstrap/js/dist/dropdown"
+import Collapse from "bootstrap/js/dist/collapse"
+import ScrollSpy from "bootstrap/js/dist/scrollspy"
+
+
+const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new Dropdown(dropdownToggleEl))
+
+const collapseElementList = document.querySelectorAll('.collapse')
+const collapseList = [...collapseElementList].map(collapseEl => new Collapse(collapseEl))
+
+const scrollSpy = new ScrollSpy(document.body, {
+  target: '#navbar-body'
+})
+
+// BATTLEFIELD SWIPER
+new Swiper('.battlefield .swiper-container', {
+  slidesPerView: 1,
+  centeredSlides: true,
+  loop: true,
+  navigation: {
+    prevEl: '.battlefield .swiper-prev',
+    nextEl: '.battlefield .swiper-next'
+  }
+});
+
+
+// footer 올해 연도 계산
+const thisYear = document.querySelector('.this-year')
+thisYear.textContent = new Date().getFullYear()
