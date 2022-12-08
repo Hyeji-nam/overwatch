68a17751d6eaa3fab1beecf28348ffbd93dcc6bd
Hyeji-nam <hyeji5245@gmail.com>
2022-12-08 22:02:59 +0900

    1차 완성

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ import Dropdown from "bootstrap/js/dist/dropdown"
 import Collapse from "bootstrap/js/dist/collapse"
 import ScrollSpy from "bootstrap/js/dist/scrollspy"
 
+import Swiper from "swiper/swiper-bundle.min.js"
+
 
 const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
 const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new Dropdown(dropdownToggleEl))
@@ -9,7 +11,7 @@ const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new Dropdo
 const collapseElementList = document.querySelectorAll('.collapse')
 const collapseList = [...collapseElementList].map(collapseEl => new Collapse(collapseEl))
 
-const scrollSpy = new ScrollSpy(document.body, {
+new ScrollSpy(document.body, {
   target: '#navbar-body'
 })
 
